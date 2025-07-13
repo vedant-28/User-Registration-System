6a72d7a634cad852c4b5203fca15b547783aa27b
vedant <vedant10vaidya@gmail.com>
2025-07-13 19:39:10 +0900

    User Registration system | config & deployment descriptor creation

---
+++ src/main/java/com/user/dao/UserDao.java
@@ -0,0 +1,4 @@
+package com.user.dao;
+
+public interface UserDao {
+}
---
+++ src/main/java/com/user/entity/User.java
@@ -0,0 +1,71 @@
+package com.user.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String city;
+
+    public User() {
+        super();
+    }
+
+    public User(int id, String name, String password, String city) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
