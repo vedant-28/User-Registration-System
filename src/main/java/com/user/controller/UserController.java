dfed0dd7de8f8778967436da8031455c917826c3
vedant <vedant10vaidya@gmail.com>
2025-07-14 21:20:59 +0900

    User Registration system | DB config & controller testing

--- src/main/java/com/user/controller/UserController.java
+++ src/main/java/com/user/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/")
+    public String test() {
+        return "index";
+    }
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,7 @@
 <html>
+    <head>
+        <title>User Registration</title>
+    </head>
 <body>
 <h2>Hello World!</h2>
 </body>
